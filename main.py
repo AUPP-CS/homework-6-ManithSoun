79b32119225ca64b52fb89233baa0bb4ee04790e
Somanith <SomanithSounexample.com>
2024-03-12 23:02:37 +0700

    1

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from weather_check import get_weather
-
 # Add your code here and remember to remove all unused comments
+
+
+while True:
+    city = input("Enter the city that you want to know: ")
+    result = get_weather(city)
+
+    
+
+    print(f'City: {get_weather(city)['name']}')
+    print(f'The condition right now: {result['description']}')
+    print(f'The temperature: {result['temp']}')
+    print(f'But feel like: {result['feel']}')
+    con = input("Do you want to continue? (Y/N) ")
+    if con.lower() == 'n':
+        print("Good bye! Don't forget to drink water to stay hydrated")
+        break
+
--- weather_check.py
+++ weather_check.py
@@ -1,9 +1,27 @@
 import requests
 
-# Reminders: 
-# - This function should return data from OpenWeather API
-# - Remove unused comments and the pass from get_weather() when you write it
-
 def get_weather(city):
-    # Add your code here
-    pass
+    api_key = 'e212f3292b421b52d5721a50a9fc0c12'
+    endpoint = 'https://api.openweathermap.org/data/2.5/weather?'
+    url = f'{endpoint}q={city}&units=metric&appid={api_key}'
+
+    response = requests.get(url).json()
+
+    # city_name = response['name']
+    city_name = response['name']
+
+    temp = response['main']['temp']
+
+    feel_like = response['main']['feels_like']
+   
+
+    description = response['weather'][0]['description']
+
+    return {
+        'temp': round(temp, 2),
+        'feel': round(feel_like, 2),
+        'description': description,
+        'name' : city_name
+       
+    }
+
